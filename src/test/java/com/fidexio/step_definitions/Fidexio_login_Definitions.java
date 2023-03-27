882f042e63351f865f4c98d40c5cfaf9b17234f2
dilekdeniz <112781926+dilekdeniz@users.noreply.github.com>
2023-03-27 20:23:07 +0100

    edited

--- src/test/java/com/fidexio/step_definitions/Fidexio_login_Definitions.java
+++ src/test/java/com/fidexio/step_definitions/Fidexio_login_Definitions.java
@@ -48,7 +48,17 @@ public class Fidexio_login_Definitions extends BasePage{
     }
 
 
+    @When("user\\(sales) enters {string} and {string} as credentials")
+    public void userSalesEntersAndAsCredentials(String username, String password) {
+        loginPage.username.sendKeys(username);
+        loginPage.password.sendKeys(password);
+    }
+
+    @Then("user\\(sales) is on the dashboard")
+    public void userSalesIsOnTheDashboard() {
+        BrowserUtils.sleep(5);
+        Assert.assertEquals("#Inbox - Odoo",Driver.getDriver().getTitle());
 
 
-
+    }
 }
